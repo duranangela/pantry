69dcf4ccff079542b9b01974fa82306ac19f668e
Angela Duran <duranangela19@gmail.com>
2018-06-05 10:15:49 -0600

    Finished Iterations 1 & 2

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,3 +1,43 @@
-class Pantry
+require 'pry'
 
+class Pantry
+  attr_reader :stock, :shopping_list
+
+  def initialize
+    @stock = {}
+    @shopping_list = {}
+  end
+
+  def stock_check(item)
+    return 0 if @stock[item].nil?
+    @stock[item]
+  end
+
+  def restock(item, count)
+    if @stock[item].nil?
+      @stock[item] = count
+    else
+      @stock[item] += count
+    end
+  end
+
+  def add_to_shopping_list(recipe)
+    recipe.ingredients.each do |item, count|
+      if @shopping_list[item].nil?
+        @shopping_list[item] = count
+      else
+        @shopping_list[item] += count
+      end
+    end
+    @shopping_list
+  end
+
+  def print_shopping_list
+    list = ''
+    @shopping_list.each do |item, count|
+      list += "* #{item}: #{count}\n"
+    end
+    print list
+    list.chomp
+  end
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -1,7 +1,68 @@
 require './lib/pantry'
 require 'minitest/autorun'
 require 'minitest/pride'
+require './lib/recipe'
+require 'pry'
 
 class PantryTest < Minitest::Test
+  def setup
+    @pantry = Pantry.new
 
+  end
+
+  def test_it_exists
+    assert_instance_of Pantry, @pantry
+  end
+
+  def test_it_has_empty_stock_by_default
+    assert_equal ({}), @pantry.stock
+  end
+
+  def test_it_can_check_pantry_for_items
+    assert_equal 0, @pantry.stock_check('Cheese')
+  end
+
+  def test_it_can_stock_and_add_items
+    @pantry.restock('Cheese', 10)
+    assert_equal 10, @pantry.stock_check('Cheese')
+    @pantry.restock('Cheese', 20)
+    assert_equal 30, @pantry.stock_check('Cheese')
+  end
+
+  def test_it_has_empty_shopping_list_by_default
+    assert_equal ({}), @pantry.shopping_list
+  end
+
+  def test_it_can_add_to_shopping_list
+    r1 = Recipe.new('Cheese Pizza')
+    r1.add_ingredient('Cheese', 20)
+    r1.add_ingredient('Flour', 20)
+    @pantry.add_to_shopping_list(r1)
+    assert_equal ({ 'Cheese' => 20, 'Flour' => 20 }), @pantry.shopping_list
+    r2 = Recipe.new('Spaghetti')
+    r2.add_ingredient('Spaghetti Noodles', 10)
+    r2.add_ingredient('Marinara Sauce', 10)
+    r2.add_ingredient('Cheese', 5)
+    @pantry.add_to_shopping_list(r2)
+    assert_equal ({
+      'Cheese' => 25,
+      'Flour' => 20,
+      'Spaghetti Noodles' => 10,
+      'Marinara Sauce' => 10
+    }), @pantry.shopping_list
+  end
+
+  def test_it_can_print_shopping_list
+    r1 = Recipe.new('Cheese Pizza')
+    r1.add_ingredient('Cheese', 20)
+    r1.add_ingredient('Flour', 20)
+    @pantry.add_to_shopping_list(r1)
+    assert_equal ({ 'Cheese' => 20, 'Flour' => 20 }), @pantry.shopping_list
+    r2 = Recipe.new('Spaghetti')
+    r2.add_ingredient('Spaghetti Noodles', 10)
+    r2.add_ingredient('Marinara Sauce', 10)
+    r2.add_ingredient('Cheese', 5)
+    @pantry.add_to_shopping_list(r2)
+    assert_equal "* Cheese: 25\n* Flour: 20\n* Spaghetti Noodles: 10\n* Marinara Sauce: 10", @pantry.print_shopping_list
+  end
 end
